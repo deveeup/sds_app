cf6f75ae809dab784b67925aeb709e0f5c62b93c
David Camacho <david.camacho@globant.com>
2023-08-20 19:52:07 -0500

    fix: getpet data.

--- src/screens/PetDetailScreen.tsx
+++ src/screens/PetDetailScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
 import { getData } from '../api/get';
 
 export default function PetDetailScreen({ route, navigation }: any) {
@@ -9,11 +9,12 @@ export default function PetDetailScreen({ route, navigation }: any) {
     })();
   }, []);
 
+  const [petData, setPetData] = useState<any>({});
   const { petId } = route.params;
   const getPet = async () => {
     try {
-      const response = await getData(`dog/SA-${petId}`);
-      console.log(response);
+      const pet = await getData(`dog/SA-${petId}`);
+      setPetData(pet);
 
     } catch(error) {
       console.error(error);
@@ -24,6 +25,19 @@ export default function PetDetailScreen({ route, navigation }: any) {
     <SafeAreaView>
       <Text>Pet Details!</Text>
       <Text>{petId}</Text>
+      <Image
+        style={styles.petImage}
+        source={{
+          uri: petData.image,
+        }}
+      />
     </SafeAreaView>
   )
 };
+
+const styles = StyleSheet.create({
+  petImage: {
+    width: 200,
+    height: 200,
+  },
+});
